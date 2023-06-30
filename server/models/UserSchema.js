40e7272a1ad99740980b05075054ce6ff32c36d1
jonnneehh <jon_llamado@dlsu.edu.ph>
2023-06-30 15:24:19 +0800

    database connection

---
+++ server/models/UserSchema.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+/* username: UNIQUE username 
+ * profilephoto: filename of their profile photo
+ * email: email of the user. validity is checked upon registering
+ * password: String of the user's password to login to site
+ */
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilephoto: {
+        type: String,
+        default: "placeholder.jpg"
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    },
+})
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
